ae047e3332f69d510d4e7b58e034cd69c08fbf0b
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2020-11-16 13:16:26 +0100

    new test added

--- jvm-count/src/main/kotlin/queue/AbstractLockFreeQueue.kt
+++ jvm-count/src/main/kotlin/queue/AbstractLockFreeQueue.kt
@@ -15,8 +15,8 @@ abstract class AbstractLockFreeQueue<T : TimestampedValue>(initValue: T) {
 
     fun pop(): T? {
         while (true) {
-            val curTail = tail.get()
             val curHead = head.get()
+            val curTail = tail.get()
 
             assert(curTail.data.timestamp >= curHead.data.timestamp)
 
--- jvm-count/src/test/kotlin/queue/NonRootLockFreeQueueTest.kt
+++ jvm-count/src/test/kotlin/queue/NonRootLockFreeQueueTest.kt
@@ -5,31 +5,37 @@ import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.annotations.Param
 import org.jetbrains.kotlinx.lincheck.check
 import org.jetbrains.kotlinx.lincheck.paramgen.IntGen
+import org.jetbrains.kotlinx.lincheck.paramgen.LongGen
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import org.jetbrains.kotlinx.lincheck.verifier.VerifierState
 import org.jetbrains.kotlinx.lincheck.verifier.linearizability.LinearizabilityVerifier
 import org.junit.jupiter.api.Test
-import kotlin.properties.Delegates
+import java.util.concurrent.atomic.AtomicLong
 
-class RootLockFreeQueueTest : VerifierState() {
+class NonRootLockFreeQueueTest : VerifierState() { // TODO: fix it
     companion object {
-        data class Dummy(val value: Int) : TimestampedValue {
-            override var timestamp by Delegates.notNull<Long>()
+        data class Dummy(val value: Int, val ts: Long) : TimestampedValue {
+            override var timestamp: Long
+                get() = ts
+                set(_) {
+                    throw UnsupportedOperationException("Set timestamp not supported")
+                }
         }
     }
 
-    private val queue: RootLockFreeQueue<Dummy>
+    private val queue: NonRootLockFreeQueue<Dummy>
 
     init {
-        val initValue = Dummy(0)
-        initValue.timestamp = 0L
-        queue = RootLockFreeQueue(initValue = initValue)
+        val initValue = Dummy(0, 0)
+        queue = NonRootLockFreeQueue(initValue = initValue)
     }
 
     @Operation
-    fun pushAndAcquireTimestamp(@Param(gen = IntGen::class, conf = "-100:100") x: Int) {
-        // TODO: make it return new timestamp
-        queue.pushAndAcquireTimestamp(Dummy(x))
+    fun push(
+        @Param(gen = IntGen::class, conf = "-100:100") x: Int,
+        @Param(gen = LongGen::class, conf = "1:1000000") ts: Long
+    ) {
+        queue.push(Dummy(x, ts))
     }
 
     @Operation

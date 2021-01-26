f3b34b08f930fb0a1cef7c46824950d07702f348
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-01-26 15:56:10 +0100

    wait-free contains tested

--- jvm-count/src/test/kotlin/tree/WaitFreeContainsTest.kt
+++ jvm-count/src/test/kotlin/tree/WaitFreeContainsTest.kt
@@ -9,20 +9,18 @@ import kotlin.random.Random
 class WaitFreeContainsTest {
     @Suppress("SameParameterValue")
     private fun doStressTest(
-        borders: List<Pair<Int, Int>>, testsCount: Int, actionPerTest: Int,
+        generators: List<(Random) -> Int>, testsCount: Int, actionPerTest: Int,
         insertProb: Double, deleteProb: Double
     ) {
         val random = Random(System.currentTimeMillis())
-        repeat(testsCount) {
-            if (it % 10 == 0) {
-                println(it)
+        repeat(testsCount) { testNum ->
+            if (testNum % 10 == 0) {
+                println(testNum)
             }
             val set = LockFreeSet<Int>()
             val threadsFinished = AtomicInteger(0)
 
-            borders.map {
-                val (curLeft, curRight) = it
-                assert(curLeft < curRight)
+            generators.map { curGen ->
                 thread {
                     val curThreadSet = HashSet<Int>()
 
@@ -30,19 +28,19 @@ class WaitFreeContainsTest {
                         val op = random.nextDouble()
                         when {
                             op <= insertProb -> {
-                                val x = random.nextInt(curLeft, curRight)
+                                val x = curGen(random)
                                 val res = set.insert(x).result
                                 val expectedRes = curThreadSet.add(x)
                                 assertEquals(expectedRes, res)
                             }
                             op <= insertProb + deleteProb -> {
-                                val x = random.nextInt(curLeft, curRight)
+                                val x = curGen(random)
                                 val res = set.delete(x).result
                                 val expectedRes = curThreadSet.remove(x)
                                 assertEquals(expectedRes, res)
                             }
                             else -> {
-                                val x = random.nextInt(curLeft, curRight)
+                                val x = curGen(random)
                                 val res = set.containsWaitFree(x)
                                 val expectedRes = curThreadSet.contains(x)
                                 assertEquals(expectedRes, res)
@@ -54,55 +52,81 @@ class WaitFreeContainsTest {
                 }
             }.forEach { it.join() }
 
-            assertEquals(threadsFinished.get(), borders.size)
+            assertEquals(threadsFinished.get(), generators.size)
         }
+    }
+
+    private fun bordersToGen(leftBorder: Int, rightBorder: Int): (Random) -> Int {
+        assert(leftBorder < rightBorder)
+        return { it.nextInt(leftBorder, rightBorder) }
     }
 
     @Test
     fun stressManyThreadsSmallKeyRange() {
-        val borders = (1..32).map { Pair(it * 20, it * 20 + 10) }
+        val generators = (1..32)
+            .map { Pair(it * 20, it * 20 + 10) }
+            .map { (left, right) -> bordersToGen(left, right) }
         doStressTest(
             testsCount = 1000,
             actionPerTest = 1000,
             insertProb = 0.2,
             deleteProb = 0.15,
-            borders = borders
+            generators = generators
         )
     }
 
     @Test
     fun stressManyThreadsWideKeyRange() {
-        val borders = (1..32).map { Pair(it * 2_000_000, it * 2_000_000 + 1_000_000) }
+        val generators = (1..32)
+            .map { Pair(it * 2_000_000, it * 2_000_000 + 1_000_000) }
+            .map { (left, right) -> bordersToGen(left, right) }
         doStressTest(
             testsCount = 1000,
             actionPerTest = 1000,
             insertProb = 0.2,
             deleteProb = 0.15,
-            borders = borders
+            generators = generators
         )
     }
 
     @Test
     fun stressFewThreadsSmallKeyRange() {
-        val borders = (1..4).map { Pair(it * 20, it * 20 + 10) }
+        val generators = (1..4)
+            .map { Pair(it * 20, it * 20 + 10) }
+            .map { (left, right) -> bordersToGen(left, right) }
         doStressTest(
             testsCount = 1000,
             actionPerTest = 1000,
             insertProb = 0.2,
             deleteProb = 0.15,
-            borders = borders
+            generators = generators
         )
     }
 
     @Test
     fun stressFewThreadsWideKeyRange() {
-        val borders = (1..4).map { Pair(it * 2_000_000, it * 2_000_000 + 1_000_000) }
+        val generators = (1..4)
+            .map { Pair(it * 2_000_000, it * 2_000_000 + 1_000_000) }
+            .map { (left, right) -> bordersToGen(left, right) }
         doStressTest(
             testsCount = 1000,
             actionPerTest = 1000,
             insertProb = 0.2,
             deleteProb = 0.15,
-            borders = borders
+            generators = generators
+        )
+    }
+
+    @Test
+    fun stressNotRanges() {
+        val genEven: (Random) -> Int = { it.nextInt(0, 1_000_000) * 2 }
+        val genOdd: (Random) -> Int = { it.nextInt(0, 1_000_000) * 2 + 1 }
+        doStressTest(
+            testsCount = 1000,
+            actionPerTest = 1000,
+            insertProb = 0.2,
+            deleteProb = 0.15,
+            generators = listOf(genEven, genOdd)
         )
     }
 }

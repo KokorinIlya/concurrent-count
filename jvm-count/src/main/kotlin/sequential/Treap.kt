1fdbafa423af912b8fa2e2ceaf05d56efdb2b3c2
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-05-05 21:24:47 +0300

    Treap added

--- jvm-count/src/main/kotlin/sequential/Treap.kt
+++ jvm-count/src/main/kotlin/sequential/Treap.kt
@@ -1,7 +1,37 @@
 package sequential
 
-class Treap<T : Comparable<T>>(val head: TreapNode<T>?) {
-    private fun split(key: T): Pair<TreapNode<T>?, TreapNode<T>?> {
-        TODO()
+import kotlin.random.Random
+
+class Treap<T : Comparable<T>>(private var head: TreapNode<T>?, private val random: Random) {
+    fun contains(key: T): Boolean {
+        var curNode = head ?: return false
+        while (true) {
+            curNode = when {
+                key == curNode.key -> return true
+                key < curNode.key -> curNode.left ?: return false
+                else -> curNode.right ?: return false
+            }
+        }
+    }
+
+    fun insert(key: T): Boolean {
+        if (contains(key)) {
+            return false
+        }
+        val (left, right) = head.split(key)
+        val keyTreap = TreapNode(key = key, priority = random.nextLong(), left = null, right = null, size = 1)
+        val curRes = merge(left, keyTreap)
+        head = merge(curRes, right)
+        return true
+    }
+
+    fun remove(key: T): Boolean {
+        if (!contains(key)) {
+            return false
+        }
+        val (splitLeft, splitRight) = head.split(key)
+        val newRight = splitRight!!.removeLeftmost()
+        head = merge(splitLeft, newRight)
+        return true
     }
 }
--- jvm-count/src/main/kotlin/sequential/TreapNode.kt
+++ jvm-count/src/main/kotlin/sequential/TreapNode.kt
@@ -1,8 +1,53 @@
 package sequential
 
 data class TreapNode<T : Comparable<T>>(
-    val data: T, val priority: Long,
+    val key: T, val priority: Long,
     val left: TreapNode<T>?,
     val right: TreapNode<T>?,
     val size: Int
-)
+) {
+    fun doCopy(
+        newKey: T = this.key, newPriority: Long = this.priority,
+        newLeft: TreapNode<T>? = this.left, newRight: TreapNode<T>? = this.right
+    ): TreapNode<T> {
+        val newSize = newLeft.getSize() + newRight.getSize() + 1
+        return TreapNode(newKey, newPriority, newLeft, newRight, newSize)
+    }
+
+    fun removeLeftmost(): TreapNode<T>? {
+        return if (left == null) {
+            right
+        } else {
+            doCopy(newLeft = left.removeLeftmost())
+        }
+    }
+}
+
+fun <T : Comparable<T>> TreapNode<T>?.getSize(): Int {
+    return this?.size ?: 0
+}
+
+fun <T : Comparable<T>> TreapNode<T>?.split(splitKey: T): Pair<TreapNode<T>?, TreapNode<T>?> {
+    return when {
+        this == null -> Pair(null, null)
+        splitKey > key -> {
+            val (splitLeft, splitRight) = right.split(splitKey)
+            val leftRes = doCopy(newRight = splitLeft)
+            Pair(leftRes, splitRight)
+        }
+        else -> {
+            val (splitLeft, splitRight) = left.split(splitKey)
+            val rightRes = doCopy(newLeft = splitRight)
+            Pair(splitLeft, rightRes)
+        }
+    }
+}
+
+fun <T : Comparable<T>> merge(left: TreapNode<T>?, right: TreapNode<T>?): TreapNode<T>? {
+    return when {
+        left == null -> right
+        right == null -> left
+        left.priority > right.priority -> left.doCopy(newRight = merge(left.right, right))
+        else -> right.doCopy(newLeft = merge(left, right.left))
+    }
+}

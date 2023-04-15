4f94d6d021521ecb6aeaadf845e348c9b3fb443f
Viktor Yudov <me@spcfox.ru>
2023-04-15 22:21:13 +0300

    Add paddings to InnerNodeContent

--- jvm-count/src/main/kotlin/tree/InnerNodeContent.kt
+++ jvm-count/src/main/kotlin/tree/InnerNodeContent.kt
@@ -1,7 +1,147 @@
 package tree
 
-class InnerNodeContent<T : Comparable<T>>(
+open class InnerNodeContentInner<T : Comparable<T>>(
     val subtreeSize: Int,
     val lastModificationTimestamp: Long,
     val modificationsCount: Int,
-)
+) : InnerNodeContentPadding()
+
+@Suppress("UNUSED")
+open class InnerNodeContentPadding {
+    private val pre00 : Byte = 0
+    private val pre01 : Byte = 0
+    private val pre02 : Byte = 0
+    private val pre03 : Byte = 0
+    private val pre04 : Byte = 0
+    private val pre05 : Byte = 0
+    private val pre06 : Byte = 0
+    private val pre07 : Byte = 0
+    private val pre08 : Byte = 0
+    private val pre09 : Byte = 0
+    private val pre10 : Byte = 0
+    private val pre11 : Byte = 0
+    private val pre12 : Byte = 0
+    private val pre13 : Byte = 0
+    private val pre14 : Byte = 0
+    private val pre15 : Byte = 0
+    private val pre16 : Byte = 0
+    private val pre17 : Byte = 0
+    private val pre18 : Byte = 0
+    private val pre19 : Byte = 0
+    private val pre20 : Byte = 0
+    private val pre21 : Byte = 0
+    private val pre22 : Byte = 0
+    private val pre23 : Byte = 0
+    private val pre24 : Byte = 0
+    private val pre25 : Byte = 0
+    private val pre26 : Byte = 0
+    private val pre27 : Byte = 0
+    private val pre28 : Byte = 0
+    private val pre29 : Byte = 0
+    private val pre30 : Byte = 0
+    private val pre31 : Byte = 0
+    private val pre32 : Byte = 0
+    private val pre33 : Byte = 0
+    private val pre34 : Byte = 0
+    private val pre35 : Byte = 0
+    private val pre36 : Byte = 0
+    private val pre37 : Byte = 0
+    private val pre38 : Byte = 0
+    private val pre39 : Byte = 0
+    private val pre40 : Byte = 0
+    private val pre41 : Byte = 0
+    private val pre42 : Byte = 0
+    private val pre43 : Byte = 0
+    private val pre44 : Byte = 0
+    private val pre45 : Byte = 0
+    private val pre46 : Byte = 0
+    private val pre47 : Byte = 0
+    private val pre48 : Byte = 0
+    private val pre49 : Byte = 0
+    private val pre50 : Byte = 0
+    private val pre51 : Byte = 0
+    private val pre52 : Byte = 0
+    private val pre53 : Byte = 0
+    private val pre54 : Byte = 0
+    private val pre55 : Byte = 0
+    private val pre56 : Byte = 0
+    private val pre57 : Byte = 0
+    private val pre58 : Byte = 0
+    private val pre59 : Byte = 0
+    private val pre60 : Byte = 0
+    private val pre61 : Byte = 0
+    private val pre62 : Byte = 0
+    private val pre63 : Byte = 0
+}
+
+@Suppress("UNUSED")
+class InnerNodeContent<T : Comparable<T>>(
+    subtreeSize: Int,
+    lastModificationTimestamp: Long,
+    modificationsCount: Int,
+) : InnerNodeContentInner<T>(subtreeSize, lastModificationTimestamp, modificationsCount) {
+    private val post00 : Byte = 0
+    private val post01 : Byte = 0
+    private val post02 : Byte = 0
+    private val post03 : Byte = 0
+    private val post04 : Byte = 0
+    private val post05 : Byte = 0
+    private val post06 : Byte = 0
+    private val post07 : Byte = 0
+    private val post08 : Byte = 0
+    private val post09 : Byte = 0
+    private val post10 : Byte = 0
+    private val post11 : Byte = 0
+    private val post12 : Byte = 0
+    private val post13 : Byte = 0
+    private val post14 : Byte = 0
+    private val post15 : Byte = 0
+    private val post16 : Byte = 0
+    private val post17 : Byte = 0
+    private val post18 : Byte = 0
+    private val post19 : Byte = 0
+    private val post20 : Byte = 0
+    private val post21 : Byte = 0
+    private val post22 : Byte = 0
+    private val post23 : Byte = 0
+    private val post24 : Byte = 0
+    private val post25 : Byte = 0
+    private val post26 : Byte = 0
+    private val post27 : Byte = 0
+    private val post28 : Byte = 0
+    private val post29 : Byte = 0
+    private val post30 : Byte = 0
+    private val post31 : Byte = 0
+    private val post32 : Byte = 0
+    private val post33 : Byte = 0
+    private val post34 : Byte = 0
+    private val post35 : Byte = 0
+    private val post36 : Byte = 0
+    private val post37 : Byte = 0
+    private val post38 : Byte = 0
+    private val post39 : Byte = 0
+    private val post40 : Byte = 0
+    private val post41 : Byte = 0
+    private val post42 : Byte = 0
+    private val post43 : Byte = 0
+    private val post44 : Byte = 0
+    private val post45 : Byte = 0
+    private val post46 : Byte = 0
+    private val post47 : Byte = 0
+    private val post48 : Byte = 0
+    private val post49 : Byte = 0
+    private val post50 : Byte = 0
+    private val post51 : Byte = 0
+    private val post52 : Byte = 0
+    private val post53 : Byte = 0
+    private val post54 : Byte = 0
+    private val post55 : Byte = 0
+    private val post56 : Byte = 0
+    private val post57 : Byte = 0
+    private val post58 : Byte = 0
+    private val post59 : Byte = 0
+    private val post60 : Byte = 0
+    private val post61 : Byte = 0
+    private val post62 : Byte = 0
+    private val post63 : Byte = 0
+}

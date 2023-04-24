3a8c116343eb6a1ae04cf4275a80f1b8feff3d92
Viktor Yudov <me@spcfox.ru>
2023-04-24 22:21:14 +0300

    Add affinity for any threads count

--- jvm-count/src/jmh/kotlin/bench/state/AffinityLockThreadState.kt
+++ jvm-count/src/jmh/kotlin/bench/state/AffinityLockThreadState.kt
@@ -1,20 +1,12 @@
 package bench.state
 
-import net.openhft.affinity.AffinityLock
 import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.ThreadParams
 
 @State(Scope.Thread)
-open class AffinityLockThreadState {
-    private lateinit var affinityLock: AffinityLock
-
-    @Setup(Level.Iteration)
-    fun init(threadParams: ThreadParams) {
-        affinityLock = AffinityLock.acquireLock(threadParams.threadIndex + 1)
-    }
-
-    @TearDown(Level.Iteration)
-    fun tearDown() {
-        affinityLock.close()
+class AffinityLockThreadState {
+    @Setup(Level.Trial)
+    fun setup(threadParams: ThreadParams, benchmarkState: AffinityLockBenchmarkState) {
+        benchmarkState.bind(threadParams.threadIndex)
     }
 }
